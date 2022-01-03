20cadf9eca943aad492e18b04c5dee0c7901677a
KoooYoung <Jestinevallendradwiputra@gmail.com>
2022-01-03 16:39:53 +0700

    added a time card and also changed the search bar behaviour, where it would only refresh if the user clicked out of the search bar or press enter

--- javascript/api.js
+++ javascript/api.js
@@ -1,9 +1,13 @@
+// get weather data
 export const weather = {
     // get all weather data and assign it into the html 1
     fetchWeatherData: function(city){
-        return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=262baac3e5df844f9992d3d71cf510cf`)
+        const apiKey1 = "262baac3e5df844f9992d3d71cf510cf"
+        const apiKey2 = "b190a0605344cc4f3af08d0dd473dd25"
+        const apiKey3 = "9a14131d21314caaf8e9208f49b174d4"
+        return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey2}`)
             .then(response => response.json())
-            .then(weatherData => this.assignData(weatherData))
+            .then(weatherData => errorFuncs.checkStatusCode(weatherData.cod, this.assignData(weatherData)))
             .catch(err => console.log(err))
     },
     assignData: function(datas){
@@ -15,8 +19,13 @@ export const weather = {
             const {country} = datas.sys
             const {lon,lat} = datas.coord
             
+            // update weather data ui
             this.updateDataUI(name,icon,main,temp,humidity,speed)     
-            this.updateMoreDataUI(visibility,feels_like,temp_min,temp_max,pressure,deg,country, lon,lat,temp,speed,humidity,name)       
+            // update more weather data ui
+            this.updateMoreDataUI(visibility,feels_like,temp_min,temp_max,pressure,deg,country, lon,lat,temp,speed,humidity,name) 
+            // call the fetchdata function      
+            this.latLong = [lat,lon]
+            times.fetchTimeData(this.latLong[0],this.latLong[1])      
         }
         catch(err){
             console.log(err)
@@ -45,13 +54,13 @@ export const weather = {
         document.querySelector(".press").textContent = `Pressure: ${pressure}`
         document.querySelector(".other-humid").textContent = `Humidity: ${humidity}%`
         document.querySelector(".country").textContent = `Country: ${country}`
-    }   
-    
+    },
+    latLong: []
 }
 
 // get user location
 export const userLocation = {
-    getUserLocation:function(){
+    getUserLocation: function(){
         // if user allows to get their location checked
         const success = async position => {
             const latitude = position.coords.latitude
@@ -79,4 +88,59 @@ export const userLocation = {
     updateWeatherToUserLoc: function(userCity){
         weather.fetchWeatherData(userCity)
     }
+}
+
+// get time data
+export const times ={
+    fetchTimeData : function (lat,long){
+        return fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=KLM734BAJD6C&format=json&by=position&lat=${lat}&lng=${long}`)
+        .then(response => response.json())
+        .then(timeData => this.assignTimeData(timeData))
+        .catch(error => console.log(error))
+    },
+    assignTimeData: function(timeData){
+        const {countryCode,countryName,zoneName,abbreviation,gmtOffset,dst,zoneStart,timestamp,formatted} = timeData 
+        const dateAndTime = formatted.split(" ")
+        // console.log(countryCode,countryName,zoneName,abbreviation,gmtOffset,dst,zoneStart,timestamp,formatted,dateAndTime)
+        this.updateTimeUI(dateAndTime[1],dateAndTime[0],abbreviation,zoneName,gmtOffset)
+        this.updateTimeDetailUI(countryCode,countryName,dst,gmtOffset,zoneStart,timestamp)
+    },
+    updateTimeUI: function(time,date,abbreviation,zoneName,gmtOffset){
+        document.querySelector(".current-time").textContent = time
+        document.querySelector(".date").textContent = date
+        document.querySelector(".timezone").textContent = `Timezone: ${abbreviation}`
+        document.querySelector(".zone").textContent = `Area Zone: ${zoneName}`
+        document.querySelector(".offset").textContent = `GMT Offset: ${gmtOffset}`
+    },
+    updateTimeDetailUI : function(countryCode,countryName,dst,gmtOffset,zoneStart,timestamp){
+        document.querySelector(".country-code").textContent = `Country Code: ${countryCode}`
+        document.querySelector(".country-name").textContent = `Country Name: ${countryName}`
+        document.querySelector(".dst").textContent = `DST: ${dst}`
+        document.querySelector(".offset-detail").textContent = `GMT Offset: ${gmtOffset}`
+        document.querySelector(".zone-start").textContent = `Zone Start: ${zoneStart}`
+        document.querySelector(".timestamp").textContent = `Timestamp: ${timestamp}`
+    }
+}
+
+// error handling functions
+export const errorFuncs ={
+    checkStatusCode: function(statusCode, functionToExecute){
+        switch (statusCode) {
+            case 429:
+                errorFuncs.tooManyRequest()
+                break;
+            default:
+                functionToExecute
+                break;
+        }
+    },
+    tooManyRequest: function(){
+        alert("Too many api requests to the server, please try again later!")
+        return errorList.error_429 = true;
+    }
+}
+
+// error list
+export const errorList = {
+    error_429: false
 }
--- javascript/main.js
+++ javascript/main.js
@@ -2,26 +2,68 @@ import * as api from "./api.js";
 
 // on load get user data and update weather based on user input
 window.addEventListener("load", function(){
+    if(api.errorList.error_429) return;
     api.weather.fetchWeatherData("NEW YORK CITY")
-    api.userLocation.getUserLocation()
+    api.userLocation.getUserLocation()    
+    weatherInteraction()
+    timeInteraction()
 })
 
 // website interaction 
-// search bar eventlistener
-document.querySelector(".search-bar").addEventListener("input", function(){
-    if(this.value.length < 2){
-        return;
-    }
-    api.weather.fetchWeatherData(this.value)
-})
+const weatherInteraction = function(){
+    // search bar eventlistener
+    document.querySelector(".search-bar").addEventListener("change", function(){
+        api.weather.fetchWeatherData(this.value)
+    })
+    document.querySelector(".search-bar").addEventListener(".keyup",function(e){
+        if(e.keyCode === 13 && this.value != ""){
+            api.weather.fetchWeatherData(this.value)
+        }
+    })
+    // more details weather buttons
+    document.querySelector("#bottom-data-weather").addEventListener("mouseover", function(){
+        document.querySelector(".details-btn-weather").setAttribute("style","opacity:1")
+    })
+    document.querySelector("#bottom-data-weather").addEventListener("mouseleave",function(){
+        document.querySelector(".details-btn-weather").setAttribute("style","opacity:0")
+    })
+    const moreDetail = document.querySelector(".more-details")
+    document.querySelector(".details-btn-weather").addEventListener("click",function(){
+        moreDetail.setAttribute("style","opacity:1; z-index: 20")
+    })
+    // close more weather details menu buttons
+    document.querySelector(".back-to-main-weather").addEventListener("click",function(){
+        moreDetail.setAttribute("style","opacity:0; z-index:-10")
+    })
 
-// more details weather buttons
-const moreDetail = document.querySelector(".more-details")
-document.querySelector(".fa-ellipsis-v").addEventListener("click",function(){
-    moreDetail.setAttribute("style","opacity:1; z-index: 20")
-})
+    // footer shake
+    document.querySelector(".footer").addEventListener("click",function(){
+        this.style.animation = "shake 0.7s ease-in-out"
+        setTimeout(()=>{this.style.animation = ""},1050)
+    })
+}
+const timeInteraction = function(){
+    // more details time details button
+    document.querySelector("#bottom-data-time").addEventListener("mouseover", function(){
+        document.querySelector(".details-btn-time").setAttribute("style","opacity:1")
+    })
+    document.querySelector("#bottom-data-time").addEventListener("mouseleave",function(){
+        document.querySelector(".details-btn-time").setAttribute("style","opacity:0")
+    })
 
-// close more details menu buttons
-document.querySelector(".fa-times-circle").addEventListener("click",function(){
-    moreDetail.setAttribute("style","opacity:0; z-index:-10")
-})
+    const moreDetailTime = document.querySelector(".more-details-time")
+    document.querySelector(".details-btn-time").addEventListener("click",function(){
+        moreDetailTime.setAttribute("style","opacity:1; z-index: 20")
+    })
+
+    // close more time details menu buttons
+    document.querySelector(".back-to-main-time").addEventListener("click",function(){
+        moreDetailTime.setAttribute("style","opacity:0; z-index:-10")
+    })
+}
+
+// refresh seconds in time
+setInterval(function(){
+    if(api.errorList.error_429) return;
+    api.times.fetchTimeData(api.weather.latLong[0], api.weather.latLong[1])
+},1000)
