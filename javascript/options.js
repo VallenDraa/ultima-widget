749ee5d0c74e5c73612bb972896fdd494146c100
KoooYoung <Jestinevallendradwiputra@gmail.com>
2022-01-04 20:24:57 +0700

    changed the button and sliders theme went lightmode is toggled, also added the resize content feature but it's still buggy. sometimes the size will increase by a lot in an abrupt manner

--- javascript/options.js
+++ javascript/options.js
@@ -1,22 +1,38 @@
 // options
-// dark/light mode
+// dark/light mode event listeners
 let lightMode = false;
 let transparent = false;
-cards = document.querySelectorAll(".card")
+const cards = document.querySelectorAll(".card")
 
 document.querySelector(".toggle-display").addEventListener("click",function(){
     document.querySelectorAll(".inner-circle")[0].classList.toggle("move-inner-circle")
     document.querySelectorAll(".toggle-btn").forEach(item => {
-        item.classList.toggle("turn-display-mode-btn")
+        item.classList.toggle("toggle-btn-change-color")
     })
     document.querySelectorAll(".toggle-btn > .fas").forEach(item => {
-        item.classList.toggle("text-white")
+        item.classList.toggle("icon-text-black")
     })
+    document.querySelectorAll(".inner-circle").forEach(item => {
+        item.classList.toggle("bg-dimgray")
+    })
+    document.querySelectorAll(".toggle-transparent>p").forEach(item => {
+        item.classList.toggle("icon-text-black")
+    })
+    document.querySelectorAll(".slider").forEach(item => {
+        item.classList.toggle("bg-lightergray")
+    })
+    document.querySelectorAll(".slider>p").forEach(item => {
+        item.classList.toggle("icon-text-black")
+    })
+
     turnLightMode()
     lightMode = !lightMode
     console.log(lightMode, "lm")
     displayTransparentConfig()
 })
+
+
+
 // text transparent
 document.querySelector(".toggle-transparent").addEventListener("click",function() {
     document.querySelectorAll(".inner-circle")[1].classList.toggle("move-inner-circle")
@@ -25,8 +41,30 @@ document.querySelector(".toggle-transparent").addEventListener("click",function(
     console.log(transparent, "tr")
 })
 
-// blinding mode function
 
+// slider eventlistener
+const rangeSliders = document.querySelectorAll(".opt-range")
+let slide1Value = 0;
+let slide2value= 0; 
+// outer spacing slider
+rangeSliders[0].addEventListener("change",function() {
+    const content= document.querySelector(".content")
+    // const style = getComputedStyle(content)
+    // let contentWidth = (parseInt(style.width)/parseInt(screen.width)) *100
+    if(this.value > slide1Value) {
+        console.log("increase")
+        content.style.width = `${70-parseInt(this.value)}%`
+    } else {
+        console.log("decrease")
+        content.style.width = `${70+parseInt(this.value)}%`
+    }
+    slide1Value = parseInt(this.value);
+})
+
+
+
+
+// blinding mode function
 function turnLightMode(){
     cards.forEach(item => {
         item.classList.toggle("light-mode-outer")
@@ -59,7 +97,6 @@ function textTransparent(){
 
     })
 }
-
 function displayTransparentConfig(){
    cards.forEach(item => {
        if(lightMode && item.classList.contains("tp-on")){
